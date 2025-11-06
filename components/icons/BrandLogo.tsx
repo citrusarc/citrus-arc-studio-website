cbacaa1518092b9329ad2c916717d60053461a22
citrusarc <citrusarc.studio@gmail.com>
2025-11-06 16:54:42 +0800

    update icon svg format

--- components/icons/BrandLogo.tsx
+++ components/icons/BrandLogo.tsx
@@ -19,16 +19,16 @@ export default function BrandLogo({
     >
       <g opacity="0.5" filter="url(#filter0_d_2018_156)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M2714.07 2399.99C3374.09 2396.79 3908.14 1860.76 3908.14 1200C3908.14 539.236 3374.09 3.20733 2714.07 0.0146484C3151.95 221.556 3452.14 675.721 3452.14 1200C3452.14 1724.28 3151.95 2178.44 2714.07 2399.99Z"
           fill={mainColor}
         />
       </g>
       <path
         opacity="0.8"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1960 0.0146484C2010.78 25.7087 2059.72 54.5318 2106.54 86.2304C2463.47 327.846 2698.07 736.526 2698.07 1200C2698.07 1663.47 2463.47 2072.15 2106.54 2313.77C2059.72 2345.47 2010.79 2374.29 1960 2399.99C2061.5 2399.5 2160.02 2386.4 2254.07 2362.2C2771.64 2228.98 3154.07 1759.15 3154.07 1200C3154.07 640.848 2771.64 171.018 2254.07 37.8056C2160.02 13.5973 2061.5 0.505611 1960 0.0146484Z"
         fill={mainColor}
       />
@@ -37,8 +37,8 @@ export default function BrandLogo({
         fill="url(#paint0_linear_2018_156)"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1800 2239.46C1707.15 2293.17 1606.43 2334.8 1500 2362.19C1404.11 2386.87 1303.59 2400 1200 2400C1198.02 2400 1196.05 2400 1194.07 2399.99C534.057 2396.79 0 1860.76 0 1200C0 539.236 534.057 3.20702 1194.07 0.0143394C1196.05 0.00478522 1198.02 0 1200 0C1303.59 0 1404.11 13.1259 1500 37.8053C1606.43 65.1988 1707.15 106.827 1800 160.539C2158.68 368.024 2400 755.831 2400 1200C2400 1644.17 2158.68 2031.98 1800 2239.46Z"
         fill="url(#paint1_linear_2018_156)"
       />
@@ -50,9 +50,9 @@ export default function BrandLogo({
           width="1202.07"
           height="2407.97"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -86,8 +86,8 @@ export default function BrandLogo({
           y2="1200"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color={gradientStart} />
-          <stop offset="1" stop-color={gradientEnd} />
+          <stop stopColor={gradientStart} />
+          <stop offset="1" stopColor={gradientEnd} />
         </linearGradient>
         <linearGradient
           id="paint1_linear_2018_156"
@@ -97,8 +97,8 @@ export default function BrandLogo({
           y2="1200"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color={gradientStart} />
-          <stop offset="1" stop-color={gradientEnd} />
+          <stop stopColor={gradientStart} />
+          <stop offset="1" stopColor={gradientEnd} />
         </linearGradient>
       </defs>
     </svg>
